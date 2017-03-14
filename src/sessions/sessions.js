0f4db06fd6e36e58b4f52a257d41533403e5d4ad
Jorge Alonso <alu0100767803@ull.edu.es>
2017-03-14 18:55:13 +0000

    Añadiendo ejemplo simple de Sessions

---
+++ src/sessions/sessions.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+
+var app = express();
+
+app.use(cookieParser());
+app.use(session({secret: "¡Shh, es un secreto!"}));
+
+app.get('/', function(req, res){
+    if(req.session.page_views){
+        req.session.page_views++;
+        res.send("Has visitado esta página " + req.session.page_views + " veces");
+    }
+    req.session.page_views = 1;
+    res.send("¡Bienvenido a esta página por primera vez!");
+});
+
+app.listen(8080);
